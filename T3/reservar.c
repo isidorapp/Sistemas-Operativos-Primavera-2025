b1d1daf2db27fa615798b5eaf24f4d8d6c2667d7
isidorapp <127784371+isidorapp@users.noreply.github.com>
2025-11-11 14:25:58 -0300

    Add files via upload

---
+++ T3/reservar.c
@@ -0,0 +1,28 @@
+#define _XOPEN_SOURCE 500
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "pss.h"
+#include "reservar.h"
+
+// Defina aca las variables globales y funciones auxiliares que necesite
+
+void initReservar() {
+  ...
+}
+
+void cleanReservar() {
+  ...
+}
+
+int reservar(int k) {
+  ...
+}
+
+void liberar(int e, int k) {
+  ...
+} 
